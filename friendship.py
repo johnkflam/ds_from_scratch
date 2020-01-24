58cd91f55ae7ec372d2540500cf43a2c2e9c2b1b
johnkflam <johnkflam2003@yahoo.com>
2020-01-24 07:28:43 -0800

    calculated avg salaries

--- friendship.py
+++ friendship.py
@@ -31,6 +31,14 @@ interests = [
     (9, "Java"), (9, "MapReduce"), (9, "Big Data")]
 
 
+salaries_and_tenures = [(83000, 8.7), (88000, 8.1),
+                        (48000, 0.7), (76000, 6),
+                        (69000, 6.5), (76000, 7.5),
+                        (60000, 2.5), (83000, 10),
+                        (48000, 1.9), (63000, 4.2)]
+
+
+
 
 def get_friendships():
     friendships = {user["id"]: [] for user in users}
