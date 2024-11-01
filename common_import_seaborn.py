73e4fad677c866682657f5d7fabd0333308da734
John Lam <johnkflam2003@yahoo.com>
2024-11-01 07:54:01 -0700

    chp04 2024.11.01

---
+++ common_import_seaborn.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+plt.style.use("seaborn-whitegrid")
+plt.rc("figure", autolayout=True)
+plt.rc(
+    "axes",
+    labelweight="bold",
+    labelsize="large",
+    titleweight="bold",
+    titlesize=14,
+    titlepad=10,
+)
